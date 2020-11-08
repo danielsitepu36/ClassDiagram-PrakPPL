5a7533e1f9e33d950883482e8d0fcc5f2ac9b884
RianWardanaPutra <f.rianwardanaputra@gmail.com>
2020-11-09 00:23:26 +0700

    modified user

--- classDiagram.py
+++ classDiagram.py
@@ -1,22 +1,60 @@
+nama_dokter = ["Sitepu", "Khrisna", "Wardana"]
 
+def logInWithGmail(email):
+    if '@gmail' in email:
+        return (True, token)
+    return (False, None)
+
+def signUpWithGmail(email):
+    return (True, token)
 
 class User:
-    def __init__(self, gmail, name, age, gender, address, role):
+    def __init__(self, gmail, name, age, gender, address, phone):
         self.gmail = gmail
         self.name = name
         self.age = age
         self.gender = gender
         self.address = address
-        self.role = role
+        self.phone = phone
+        if name in nama_dokter:
+            self.role = "dokter"
+        elif name in list_admin:
+            self.role = "admin"
+        else:
+            self.role = "user"
         self.isLogin = False
+        self.id = None
 
     def login(self, gmail):
-        if(logInWithGmail(gmail)):
+        login, ID = logInWithGmail(gmail)
+        if(login):
             self.isLogin = True
+            self.id = ID
             return True
         else:
             return False
-    
+
+    def signup(self, gmail):
+        login, ID = signUpWithGmail(gmail)
+        if login:
+            self.isLogin = True
+            self.id = ID
+
     def logout(self):
         self.isLogin = False
+        self.id = None
         return True
+
+    def updateIdentity(self, name, age, gender, address, phone):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.address = address
+        self.phone = phone
+
+class Pasien(User):
+    def __init__(self):
+        super().__init__()
+        self.role = "pasien"
+
+
